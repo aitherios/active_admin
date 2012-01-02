419e2e36fbd76d7ef4f78c9cc56bdb9221e39c56
Julio Protzek <julioprotzek@gmail.com>
2012-01-02 10:12:16 -0200

    changing cucumber steps to be less brittle

--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -1,5 +1,5 @@
 Then /^I should see the default welcome message$/ do
-  Then %{I should see "Welcome to Active Admin" within "#dashboard_default_message"}
+  Then %{I should see "Welcome to Active Admin"}
 end
 
 Then /^I should not see the default welcome message$/ do
@@ -7,9 +7,9 @@ Then /^I should not see the default welcome message$/ do
 end
 
 Then /^I should see a dashboard widget "([^"]*)"$/ do |name|
-  page.should have_css('.dashboard .panel h3', :text => name) 
+  page.should have_css('.dashboard .panel h3', :text => name)
 end
 
 Then /^I should not see a dashboard widget "([^"]*)"$/ do |name|
-  page.should_not have_css('.dashboard .panel h3', :text => name) 
+  page.should_not have_css('.dashboard .panel h3', :text => name)
 end
