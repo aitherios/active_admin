02ddfed8a34afcf38635e2d5701981d87dbac85c
Julio Protzek <julioprotzek@gmail.com>
2012-01-02 17:41:49 -0200

    bugfix: changing require order

--- app/assets/javascripts/active_admin/base.js
+++ app/assets/javascripts/active_admin/base.js
@@ -5,8 +5,8 @@
 //= require bootstrap/buttons
 //= require bootstrap/dropdown
 //= require bootstrap/modal
-//= require bootstrap/popover
 //= require bootstrap/scrollspy
 //= require bootstrap/tabs
 //= require bootstrap/twipsy
+//= require bootstrap/popover
 //= require active_admin/application
