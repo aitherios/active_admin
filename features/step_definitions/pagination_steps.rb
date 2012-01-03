ff5433173dd7561e7b38b7b47b52db7f9e0dc9c8
Julio Protzek <julioprotzek@gmail.com>
2012-01-03 13:25:24 -0200

    making cucumber specs pass

--- features/step_definitions/pagination_steps.rb
+++ features/step_definitions/pagination_steps.rb
@@ -3,6 +3,5 @@ Then /^I should not see pagination$/ do
 end
 
 Then /^I should see pagination with (\d+) pages$/ do |count|
-  Then %{I should see "#{count}" within ".pagination a"}
-  Then %{I should not see "#{count.to_i + 1}" within ".pagination a"}
+  page.should have_css(".pagination .page", count: count)
 end
