ad3bb9bf7bf4f1c202499f1cdb24b55d27b30e85
Julio Protzek <julioprotzek@gmail.com>
2012-01-05 16:13:20 -0200

    image uploader styles and js

--- app/assets/javascripts/active_admin/application.js
+++ app/assets/javascripts/active_admin/application.js
@@ -7,7 +7,26 @@ $(function(){
     return false;
   });
   
-  // yeah I know...
+  // yeah I know... I will refactor it sometime
    $('.sidebar.modal form .filter_form_field').wrapAll('<div class="modal-body"/>');
    $('.sidebar.modal form .filter_form_field label').removeClass('label');
+   
+  // thumbnails on forms
+  $('form .thumbnail').each(function(){
+    var img = $(this).find('img');
+    
+    if (img.length) {
+      img.appendTo($(this).find('div:first'));
+      img.wrap('<figure/>');
+      img.parent().append('<a class="remove">Remove</a>');
+  
+      var that= this;
+      $(this).find('.remove').click(function(){
+        $(that).find('figure').hide();
+        $(that).find('input').show()
+      });
+      
+      $(this).find('input').hide()
+    }
+  });
 });
